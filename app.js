5646cbe8f526704cf109c12f561969510ba65465
Karlo Dujmic <kd48376@oss.unist.hr>
2023-01-12 16:49:09 +0100

    Test sequelize connection

--- app.js
+++ app.js
@@ -1,18 +1,21 @@
 import express from 'express';
 import morgan from 'morgan';
 import dotenv from 'dotenv';
-
+import sequelize from './database/client.js';
 
 dotenv.config({ path: './config.env' });
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 app.use(express.json());
 app.use(morgan('dev'));
 
+sequelize
+	.authenticate()
+	.then(() => console.log('Database connected'))
+	.catch((err) => console.log(err.message));
 
 app.listen(port, () => {
 	console.log(`Server listening on the port  ${port}`);
 });
-;
