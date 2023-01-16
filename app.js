549c4fdd2cdf114518ea7d0a7f2c4d8012d97f6e
Karlo Dujmic <karlo.dujmic@agilathon.com>
2023-01-16 17:12:06 +0100

    Add default route for professorRouter to app.js

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
-import express from 'express';
-import morgan from 'morgan';
-import dotenv from 'dotenv';
-import sequelize from './database/client.js';
+const express = require('express');
+const morgan = require('morgan');
+const dotenv = require('dotenv');
+const db = require('./models');
+
+const professorRouter = require('./routers/professorRouter');
 
 dotenv.config({ path: './config.env' });
 
@@ -11,10 +13,11 @@ const port = process.env.PORT || 8080;
 app.use(express.json());
 app.use(morgan('dev'));
 
-sequelize
-	.authenticate()
+db.sequelize.authenticate()
 	.then(() => console.log('Database connected'))
 	.catch((err) => console.log(err.message));
+
+app.use('/professor', professorRouter);
 
 app.listen(port, () => {
 	console.log(`Server listening on the port  ${port}`);
