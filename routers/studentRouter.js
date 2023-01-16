df341f95f72f9f49b01e832c85345237b2e9c3dd
Karlo Dujmic <karlo.dujmic@agilathon.com>
2023-01-16 17:36:34 +0100

    Create router for "Student" model

---
+++ routers/studentRouter.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const studentController = require('../controllers/studentController');
+
+const router = express.Router({ mergeParams: true });
+
+router
+	.route('/')
+	.get(studentController.findAllStudents)
+	.post(studentController.createStudent);
+
+router
+	.route('/:id')
+	.get(studentController.findStudent)
+	.put(studentController.updateStudent)
+	.delete(studentController.deleteStudent);
+
+module.exports = router;
