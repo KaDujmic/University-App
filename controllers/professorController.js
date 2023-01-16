bab648f75c5432d3d891a150f29eef0239ffff20
Karlo Dujmic <karlo.dujmic@agilathon.com>
2023-01-16 17:10:32 +0100

    Create basic CRUD functions for "Professor" model

---
+++ controllers/professorController.js
@@ -0,0 +1,50 @@
+const models = require('../models');
+
+exports.findAllProfessors = async (req, res) => {
+	try {
+		const professors = await models.Professor.findAll();
+		res.status(200).json({ professors });
+	} catch (err) {
+		res.status(404).json(err.message);
+	}
+};
+
+exports.findProfessor = async (req, res) => {
+	try {
+		const professor = await models.Professor.findByPk(req.params.id);
+		res.status(200).json(professor);
+	} catch (err) {
+		res.status(404).json(err.message);
+	}
+};
+
+exports.createProfessor = async (req, res) => {
+	try {
+		const professor = await models.Professor.create(req.body);
+		res.status(200).json(professor);
+	} catch (err) {
+		res.status(404).json(err.message);
+	}
+};
+
+exports.updateProfessor = async (req, res) => {
+	try {
+		const professor = await models.Professor.update(req.body, {
+			where: { id: req.params.id },
+		});
+		res.status(200).json(professor);
+	} catch (err) {
+		res.status(404).json(err.message);
+	}
+};
+
+exports.deleteProfessor = async (req, res) => {
+	try {
+		const professor = await models.Professor.destroy({
+			where: { id: req.params.id },
+		});
+		res.status(200).json(professor);
+	} catch (err) {
+		res.status(404).json(err.message);
+	}
+};
