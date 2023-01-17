01ab3400ba7870035e62edd8eb548ea8e834f3f9
Karlo Dujmic <karlo.dujmic@agilathon.com>
2023-01-17 16:54:37 +0100

    Update studentsOnMajor function with sequelize

--- controllers/majorController.js
+++ controllers/majorController.js
@@ -52,18 +52,23 @@ exports.deleteMajor = async (req, res) => {
 
 exports.studentsOnMajor = async (req, res) => {
 	try {
-		const students = await sequelize.query(
-			`
-      select 
-        s."fullName" 
-      from "Students" s 
-      join "Majors" m ON s."majorId" = m.id 
-      where m.id = ${req.params.id}
-      `
-		);
+		// const students = await sequelize.query(
+		// 	`
+		//   select
+		//     s."fullName"
+		//   from "Students" s
+		//   join "Majors" m ON s."majorId" = m.id
+		//   where m.id = ${req.params.id}
+		//   `
+		// );
+		const students = await models.Student.findAll({
+			attributes: ['fullName', 'Major.name'],
+			include: [models.Major],
+			where: { majorId: req.params.id },
+		});
 		res
 			.status(200)
-			.json(students[0].map((student) => student.fullName));
+			.json(students);
 	} catch (err) {
 		res.status(404).json(err.message);
 	}
