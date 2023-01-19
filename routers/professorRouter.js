e554dd98a3e70ffee72e4b08ed513b409cde2975
Karlo Dujmic <karlo.dujmic@agilathon.com>
2023-01-19 10:50:36 +0100

    Update professorRouter to restrict all routes if professor is not logged in

--- routers/professorRouter.js
+++ routers/professorRouter.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const professorController = require('../controllers/professorController');
+const authController = require('../controllers/authController');
 
 const router = express.Router({ mergeParams: true });
+
+router.post('/login', authController.login);
+
+router.use(authController.protect);
 
 router
 	.route('/')
