434640c60dd8c0786b03c80973f98c7e87b70eba
Karlo Dujmic <karlo.dujmic@agilathon.com>
2023-01-20 17:55:39 +0100

    Add restriction to logged in users and role

--- routers/professorRouter.js
+++ routers/professorRouter.js
@@ -8,17 +8,35 @@ router.use(authController.isLoggedIn);
 
 router
 	.route('/')
-	.get(professorController.findAllProfessors)
-	.post(professorController.createProfessor);
+	.get(
+		authController.restrictTo('professor'),
+		professorController.findAllProfessors
+	)
+	.post(
+		authController.restrictTo('professor'),
+		professorController.createProfessor
+	);
 
 router
 	.route('/:id')
-	.get(professorController.findProfessor)
-	.put(professorController.updateProfessor)
-	.delete(professorController.deleteProfessor);
+	.get(
+		authController.restrictTo('professor'),
+		professorController.findProfessor
+	)
+	.put(
+		authController.restrictTo('professor'),
+		professorController.updateProfessor
+	)
+	.delete(
+		authController.restrictTo('professor'),
+		professorController.deleteProfessor
+	);
 
 router
 	.route('/:id/courses')
-	.get(professorController.professorCourses);
+	.get(
+		authController.restrictTo('professor'),
+		professorController.professorCourses
+	);
 
 module.exports = router;
