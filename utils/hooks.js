149a1ec4bbe9e63133e0744c3fd1f6c16d0569b9
Karlo Dujmic <karlo.dujmic@agilathon.com>
2023-01-22 14:20:52 +0100

    Add UUID create function and email check functions

--- utils/hooks.js
+++ utils/hooks.js
@@ -1,4 +1,5 @@
 const bcrypt = require('bcrypt');
+const { v4: uuidv4 } = require('uuid');
 
 (exports.isUpdateId = (data, options) => {
 	if (data.attributes.id) {
@@ -17,8 +18,32 @@ const bcrypt = require('bcrypt');
 		}
 	});
 
+exports.createUUID = (data, options) => {
+	data.id = uuidv4();
+};
+
 exports.hashPassword = async (data, options) => {
 	data.password = await bcrypt.hash(data.password, 2);
 	console.log(data.password);
 	return data.save();
+};
+
+exports.userEmailCheckStudent = async (sequelize, email) => {
+	const user = await sequelize.models.Professor.findAll({
+		where: { email: email },
+	});
+	if (user)
+		throw new Error(
+			'User with that email exists, please use different email!'
+		);
+};
+
+exports.userEmailCheckProfessor = async (sequelize, email) => {
+	const user = await sequelize.models.Student.findAll({
+		where: { email: email },
+	});
+	if (user)
+		throw new Error(
+			'User with that email exists, please use different email!'
+		);
 };
