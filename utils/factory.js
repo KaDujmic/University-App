316918a43d0dfa72a6a8654c2f2318f750276446
Karlo Dujmic <karlo.dujmic@agilathon.com>
2023-01-24 10:58:39 +0100

    Update status codes in default crud functions

--- utils/factory.js
+++ utils/factory.js
@@ -1,6 +1,8 @@
 exports.findAllModel = async (Model, req, res) => {
 	try {
-		const models = await Model.findAll();
+		const models = await Model.findAll({
+			hooks: true,
+		});
 		res.status(200).json({ models });
 	} catch (err) {
 		res.status(404).json(err.message);
@@ -11,6 +13,7 @@ exports.findModel = async (Model, req, res) => {
 	try {
 		const model = await Model.findOne({
 			where: { id: req.params.id },
+			hooks: true,
 		});
 		res.status(200).json(model);
 	} catch (err) {
@@ -22,7 +25,7 @@ exports.findModel = async (Model, req, res) => {
 exports.createModel = async (Model, req, res) => {
 	try {
 		const model = await Model.create(req.body);
-		res.status(200).json(model);
+		res.status(201).json(model);
 	} catch (err) {
 		const statusCode = err.statusCode || 500;
 		res.status(statusCode).json(err.message);
@@ -46,7 +49,7 @@ exports.deleteModel = async (Model, req, res) => {
 		const model = await Model.destroy({
 			where: { id: req.params.id },
 		});
-		res.status(200).json(model);
+		res.status(204).json(model);
 	} catch (err) {
 		const statusCode = err.statusCode || 500;
 		res.status(statusCode).json(err.message);
