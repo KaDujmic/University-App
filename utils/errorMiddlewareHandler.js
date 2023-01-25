bc5a9f13a01c6fcebe4bdd2639936201ee5bbddc
Karlo Dujmic <karlo.dujmic@agilathon.com>
2023-01-25 10:45:05 +0100

    Create error handling middleware

---
+++ utils/errorMiddlewareHandler.js
@@ -0,0 +1,25 @@
+const callbackErrorHandler = (callback) => {
+	return (req, res, next) => callback(req, res, next).catch(next);
+};
+
+const errorMiddleware = async (err, req, res, next) => {
+	if (res.headersSent) {
+		return next(err);
+	}
+	if (err.name === 'ValidationError') {
+		console.log(err);
+		res.status(err.statusCode).json({ message: err.message });
+	} else if (err.name === 'AuthorizationError') {
+		res.status(err.statusCode).json({ message: err.message });
+	} else if (err.name === 'NotFoundError') {
+		res.status(err.statusCode).json({ message: err.message });
+	} else {
+		console.error(err);
+		res.status(err.statusCode ?? 500).json({
+			message: err.message ?? 'Oops, something went wrong!',
+		});
+	}
+};
+
+module.exports.errorMiddleware = errorMiddleware;
+module.exports.callbackErrorHandler = callbackErrorHandler;
