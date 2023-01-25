0e9fa57a3d5b3a2e4b2c27be29408f58b1e5fe00
Karlo Dujmic <karlo.dujmic@agilathon.com>
2023-01-25 10:46:55 +0100

    Remove try catch blocks from all functions

--- utils/factory.js
+++ utils/factory.js
@@ -1,69 +1,34 @@
 exports.findAllModel = async (Model, req, res) => {
-	try {
-		const models = await Model.findAll({
-			hooks: true,
-		});
-		res.status(200).json({ models });
-	} catch (err) {
-		res.status(err.statusCode ?? 500).json({
-			message: err.message ?? 'Oops, something went wrong!',
-		});
-	}
+	const models = await Model.findAll({
+		hooks: true,
+	});
+	res.status(200).json({ models });
 };
 
 exports.findModel = async (Model, req, res) => {
-	try {
-		const model = await Model.findOne({
-			where: { id: req.params.id },
-			hooks: true,
-		});
-		res.status(200).json(model);
-	} catch (err) {
-		res.status(err.statusCode ?? 500).json({
-			message: err.message ?? 'Oops, something went wrong!',
-		});
-	}
+	const model = await Model.findOne({
+		where: { id: req.params.id },
+		hooks: true,
+	});
+	res.status(200).json(model);
 };
 
 exports.createModel = async (Model, req, res) => {
-	try {
-		const model = await Model.create(req.body);
-		res.status(201).json(model);
-	} catch (err) {
-		res
-			.status(err.statusCode ?? 500)
-			.json({
-				message: err.message ?? 'Oops, something went wrong!',
-			});
-	}
+	const model = await Model.create(req.body);
+	res.status(201).json(model);
 };
 
 exports.updateModel = async (Model, req, res) => {
-	try {
-		const model = await Model.update(req.body, {
-			where: { id: req.params.id },
-		});
-		res.status(200).json(model);
-	} catch (err) {
-		res
-			.status(err.statusCode ?? 500)
-			.json({
-				message: err.message ?? 'Oops, something went wrong!',
-			});
-	}
+	const model = await Model.update(req.body, {
+		where: { id: req.params.id },
+		returning: true,
+	});
+	res.status(200).json(model);
 };
 
 exports.deleteModel = async (Model, req, res) => {
-	try {
-		const model = await Model.destroy({
-			where: { id: req.params.id },
-		});
-		res.status(204).json(model);
-	} catch (err) {
-		res
-			.status(err.statusCode ?? 500)
-			.json({
-				message: err.message ?? 'Oops, something went wrong!',
-			});
-	}
+	const model = await Model.destroy({
+		where: { id: req.params.id },
+	});
+	res.status(204).json(model);
 };
