ab1a4cc0112b99e955766c8a36c8487d782887ec
Karlo Dujmic <karlo.dujmic@agilathon.com>
2023-02-03 12:57:21 +0100

    Add schemas to swagger.js

--- swagger.js
+++ swagger.js
@@ -1,12 +1,114 @@
 const swaggerAutogen = require('swagger-autogen')({ openapi: '3.0.0' });
 
 const doc = {
-    info: {
-      title: 'Unit test demo',
-      description: 'Description'
-    },
-    host: 'localhost:4000',
-    schemes: ['http']
+  info: {
+    title: 'University App',
+    description: 'University App Documentation of endpoints with request, body and response examples'
+  },
+  host: 'localhost:4000',
+  schemes: ['http'],
+  components: {
+    '@schemas': {
+      ValidationErrorDetail: {
+        type: 'object',
+        properties: {
+          msg: {
+            type: 'string'
+          },
+          param: {
+            type: 'string'
+          },
+          location: {
+            type: 'string'
+          }
+        }
+      },
+      ValidationError: {
+        type: 'object',
+        properties: {
+          message: {
+            type: 'string'
+          }
+        }
+      },
+      NotFoundError: {
+        type: 'object',
+        properties: {
+          message: {
+            type: 'string'
+          }
+        }
+      },
+      AuthorizationError: {
+        type: 'object',
+        properties: {
+          message: {
+            type: 'string'
+          }
+        }
+      },
+      StudentBody: {
+        type: 'object',
+        properties: {
+          full_name: {
+            type: 'string'
+          },
+          email: {
+            type: 'string',
+            format: 'email'
+          },
+          address: {
+            type: 'string'
+          },
+          phone_number: {
+            type: 'string'
+          },
+          major_id: {
+            type: 'string',
+            format: 'uuid'
+          },
+          password: {
+            type: 'string',
+            format: 'password'
+          }
+        }
+      },
+      StudentResponse: {
+        type: 'object',
+        properties: {
+          id: {
+            type: 'string',
+            format: 'uuid'
+          },
+          full_name: {
+            type: 'string'
+          },
+          email: {
+            type: 'string',
+            format: 'email'
+          },
+          address: {
+            type: 'string'
+          },
+          phone_number: {
+            type: 'string'
+          },
+          major_id: {
+            type: 'string',
+            format: 'uuid'
+          },
+          createdAt: {
+            type: 'string',
+            format: 'date-time'
+          },
+          updatedAt: {
+            type: 'string',
+            format: 'date-time'
+          }
+        }
+      }
+    }
+  }
 };
 
 const outputFile = './swagger-output.json';
